4a42411c2826c8213b42ce2c24b13af0cc6281c1
Kannan Muthiah <kannan.muthiah@yahoo.com>
2021-09-26 20:12:50 -0400

    Add query param "action"

--- src/main/java/kmuthiah/string/v1/controllers/StringApiController.java
+++ src/main/java/kmuthiah/string/v1/controllers/StringApiController.java
@@ -4,12 +4,13 @@ import kmuthiah.string.v1.api.UtilitiesApi;
 import kmuthiah.string.v1.models.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class StringApiController implements UtilitiesApi {
     @Override
-    public ResponseEntity<Response> utilities() {
-        return new ResponseEntity<>(new Response().text("Hello World!"), HttpStatus.OK);
+    public ResponseEntity<Response> utilities(@RequestParam(value = "action", required = true) String action) {
+        return new ResponseEntity<>(new Response().text("Here is your action:" + action), HttpStatus.OK);
     }
 }
